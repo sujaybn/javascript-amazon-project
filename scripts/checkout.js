6313c0b482e34b9887b8a88808ed8f0a74e3f138
sujaybn <sujaybagur@icloud.com>
2025-06-04 12:48:03 +0530

    fixed price fromatting and radio buttons

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
-
+import { formatCurency } from "./utils/money.js";
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem)=>{
@@ -31,7 +31,7 @@ cart.forEach((cartItem)=>{
             ${matchingProduct.name}
           </div>
           <div class="product-price">
-            $${(matchingProduct.priceCents/100).toFixed(2)}
+            $${formatCurency(matchingProduct.priceCents)}
           </div>
           <div class="product-quantity">
             <span>
@@ -53,7 +53,7 @@ cart.forEach((cartItem)=>{
           <div class="delivery-option">
             <input type="radio" checked
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingProduct.id}">
             <div>
               <div class="delivery-option-date">
                 Tuesday, June 21
@@ -66,7 +66,7 @@ cart.forEach((cartItem)=>{
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingProduct.id}">
             <div>
               <div class="delivery-option-date">
                 Wednesday, June 15
@@ -79,7 +79,7 @@ cart.forEach((cartItem)=>{
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingProduct.id}">
             <div>
               <div class="delivery-option-date">
                 Monday, June 13
